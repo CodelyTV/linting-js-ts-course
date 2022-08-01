e89f09f77ca4c40669c175a6a7399f59d867096b
Nuria <soriano.nuria@gmail.com>
2022-08-01 13:01:24 +0200

    lesson 1-3: basic config step 3 extend and plugin

--- 1-3-basic-config/.eslintrc.js
+++ 1-3-basic-config/.eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   "parser": "@typescript-eslint/parser",
+  "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  "plugins": ["@typescript-eslint"],
   "rules": {
     "semi": ["error", "always"],
-    "quotes": ["error", "double"]
+    "quotes": ["error", "double"],
+    "@typescript-eslint/no-non-null-assertion": "off"
   },
 };
