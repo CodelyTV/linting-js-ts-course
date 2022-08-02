54c3b3f0c7996083077a77c3f04d3a198e486098
Nuria <soriano.nuria@gmail.com>
2022-08-02 12:58:06 +0200

    lesson 3-2: good practises step 1: add rules

--- 3-2-good-practises/.eslintrc.js
+++ 3-2-good-practises/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   ],
   plugins: ["@typescript-eslint", "simple-import-sort", "import", "unused-imports", "editorconfig"],
   rules: {
+    //error prevention
     "array-callback-return": ["error", { checkForEach: true }],
     "no-await-in-loop": "error",
     "no-constant-binary-expression": "error",
@@ -24,8 +25,33 @@ module.exports = {
     "no-unused-private-class-members": "error",
     "no-use-before-define": "error",
     "require-atomic-updates": "error",
+
+    // good practises
+    camelcase: "error",
+    eqeqeq: "error",
+    "new-cap": "error",
+    "no-array-constructor": "error",
+    "no-console": ["error", { allow: ["error"] }],
+    "no-else-return": ["error", { allowElseIf: false }],
+    "no-extend-native": "error",
+    "no-lonely-if": "error",
+    "no-param-reassign": "error",
+    "no-return-assign": "error",
+    "no-throw-literal": "error",
+    "no-var": "error",
+    "object-shorthand": "error",
+    "prefer-const": "error",
+    "prefer-rest-params": "error",
+    "prefer-spread": "error",
+    "prefer-template": "error",
+    radix: "error",
+    yoda: "error",
+
+    // style
     semi: ["error", "always"],
     quotes: ["error", "double"],
+
+    // plugins
     "import/first": "error",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
