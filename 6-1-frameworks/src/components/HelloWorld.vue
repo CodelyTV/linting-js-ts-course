e18fecf6803e8f1a8c5d18662c1a73fbc4e0f296
Nuria <soriano.nuria@gmail.com>
2022-09-13 10:40:15 +0200

    lesson 6-1: frameworks end

--- 6-1-frameworks/src/components/HelloWorld.vue
+++ 6-1-frameworks/src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ function sayHello() {
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
-    <button v-on:click="sayHello">Say hello!</button>
+    <button @click="sayHello">Say hello!</button>
   </div>
 </template>
 
