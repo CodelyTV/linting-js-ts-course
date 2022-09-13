c325686d3f50ff8bd08f16bb073261ab3d39e079
Nuria <soriano.nuria@gmail.com>
2022-09-13 12:26:00 +0200

    lesson 6-2: testing start

---
+++ 6-2-testing/.eslintrc.js
@@ -0,0 +1,83 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  plugins: ["simple-import-sort", "unused-imports", "import", "editorconfig"],
+  rules: {
+    //error prevention
+    "array-callback-return": ["error", { checkForEach: true }],
+    "no-await-in-loop": "error",
+    "no-constant-binary-expression": "error",
+    "no-constructor-return": "error",
+    "no-promise-executor-return": "error",
+    "no-self-compare": "error",
+    "no-template-curly-in-string": "error",
+    "no-unmodified-loop-condition": "error",
+    "no-unreachable-loop": "error",
+    "no-unused-private-class-members": "error",
+    "no-use-before-define": "error",
+    "require-atomic-updates": "error",
+
+    // good practises
+    camelcase: "error",
+    eqeqeq: "error",
+    "new-cap": "error",
+    "no-array-constructor": "error",
+    "no-console": ["error", { allow: ["error"] }],
+    "no-else-return": ["error", { allowElseIf: false }],
+    "no-extend-native": "error",
+    "no-lonely-if": "error",
+    "no-param-reassign": "error",
+    "no-return-assign": "error",
+    "no-throw-literal": "error",
+    "no-var": "error",
+    "object-shorthand": "error",
+    "prefer-const": "error",
+    "prefer-rest-params": "error",
+    "prefer-spread": "error",
+    "prefer-template": "error",
+    radix: "error",
+    yoda: "error",
+
+    // style
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+
+    // plugins
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "import/no-restricted-paths": [
+      "error",
+      {
+        zones: [{ target: "./src", from: "./ui" }],
+      },
+    ],
+    "import/no-webpack-loader-syntax": "error",
+    "simple-import-sort/exports": "error",
+    "simple-import-sort/imports": "error",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+  ignorePatterns: ["**/*.html"],
+  overrides: [
+    {
+      files: ["tests/*.spec.js"],
+      extends: ["plugin:jest/recommended", "plugin:jest/style"],
+    },
+  ],
+};
---
+++ 6-2-testing/src/users.js
@@ -0,0 +1,3 @@
+export async function getUsers() {
+  return await [{ name: "Rafa" }, { name: "Núria" }];
+}
